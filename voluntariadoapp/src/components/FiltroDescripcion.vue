714f6c7f7d2659da59cb49fac25de390c3fd788c
Sebastián Órdenes <sebastian.ordenes@usach.cl>
2020-12-24 00:45:20 -0300

    Filtros descripcion funcionando
    se filtra por nombre y por descripcion por separado(ojo)

--- voluntariadoapp/src/components/FiltroDescripcion.vue
+++ voluntariadoapp/src/components/FiltroDescripcion.vue
@@ -4,49 +4,73 @@
 
 <button v-on:click="getUsers" class="btn btn-primary" v-if="!Lconcat.length">Filtro por descripcion</button>
 
-<ul class="list-group" v-else>
 
-    <div class="col">       
-     <li class="list-group-item">
-            <div class="col">
-                 <input type="text" placeholder="Buscar por descripcion" class="form-control" v-model="descripcion">
-             </div>
-     </li>
-     </div>
-     <div class="col">   
-<table class="table">
-    <thead class="table-dark">
-        <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Nombre</th>
-            <th scope="col" >Descripcion</th>
-            <th scope="col" >Fecha de creacion</th>
-        </tr>
-    </thead> 
+    <ul class="list-group" v-else>
+
+        <div class="col">       
+        <li class="list-group-item">
+                <div class="col">
+                    <input type="text" placeholder="Buscar por nombre" class="form-control" v-model="nombre"> <br>
+                    <input type="text" placeholder="Buscar por descripcion" class="form-control" v-model="descripcion"> <br>
+                  
+                    
+                </div>
+        </li>
+        </div>
+        <div class="col">
+
+ 
+    <table class="table">
+    
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col" >Descripcion</th>
+                <th scope="col" >Fecha de creacion</th>
+            </tr>
+        </thead> 
 
 
-
+<template v-if="this.nombre == '' ">
     <li v-for="item in searchUser" class="list-group-item" :key="item">
       <tbody>
        
              <tr>
            
                   <td scope="col">{{item.id}}</td>
-                  <td>{{item.emergencia}}</td>
-                    <td>{{item.tarea}}</td>
+                  <td scope="col">{{item.nombre}}</td>
                     <td scope="col">{{item.descrip}}</td>
                         <td scope="col">{{item.fecha_de_creacion}}</td>
            
              </tr>
        
-    </tbody>
-       </li>
-  
-</table>
- </div>
-</ul>
-</div>
-  </div>
+      </tbody>
+    </li>
+    </template>
+<template v-else-if="this.descripcion == '' ">
+     <li v-for="item in searchName" class="list-group-item" :key="item">
+      <tbody>
+       
+             <tr>
+           
+                  <td scope="col">{{item.id}}</td>
+                  <td scope="col">{{item.nombre}}</td>
+                    <td scope="col">{{item.descrip}}</td>
+                        <td scope="col">{{item.fecha_de_creacion}}</td>
+           
+                        </tr>
+                
+                </tbody>
+                </li>
+            </template>
+                
+    </table>
+    </div>
+    
+    </ul>
+    </div>
+    </div>
 </template>
 
 
@@ -92,10 +116,13 @@
      
         computed: {
             searchUser: function () {
-                return this.Lconcat.filter((item) =>((item.descrip.includes(this.descripcion)) || (item.tarea.includes(this.descripcion))) );
+                return this.Lconcat.filter((item) =>(item.descrip.includes(this.descripcion) ) );
 
             },
+         searchName: function () {
+                return this.Lconcat.filter((item) =>(item.nombre.includes(this.nombre) ) );
 
+            },
           
 
         }};
